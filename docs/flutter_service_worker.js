175faf475734085feb1d5f4a6d712ee594cc7e2d
StudioLuma <servus@alpenkamm-traunstein.com>
2026-03-01 10:53:57 +0100

    Add files via upload

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "58f61c3ff8fbedbc0167753cd21f976b
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e1021a367ed623c953c5e476aefce2a3",
+"flutter_bootstrap.js": "3520359bfb15c7726191db401dc3728c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
